620b3192619f844de88fdc9eca1f74cf3fde6c0e
Pierre <mfpierre@gmail.com>
2014-08-05 23:03:04 +0200

    Add an isometric example

---
+++ examples/isometric_rpg/js/game.js
@@ -0,0 +1,47 @@
+/**
+ * main
+ */
+var game = {
+
+    /**
+     *
+     * Initialize the application
+     */
+    onload: function() {
+
+        // init the video
+        if (!me.video.init('game', me.video.CANVAS, 800, 600, true, 'auto')) {
+            alert("Sorry but your browser does not support html 5 canvas. Please try with another one!");
+            return;
+        }
+
+        // set all ressources to be loaded
+        me.loader.onload = this.loaded.bind(this);
+
+        // set all ressources to be loaded
+        me.loader.preload(game.resources);
+
+        // load everything & display a loading screen
+        me.state.change(me.state.LOADING);
+    },
+
+
+    /**
+     * callback when everything is loaded
+     */
+    loaded: function ()    {
+
+        // set the "Play/Ingame" Screen Object
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+
+        // set the fade transition effect
+        me.state.transition("fade","#FFFFFF", 250);
+
+        // register our objects entity in the object pool
+        me.pool.register("mainPlayer", game.PlayerEntity);
+
+        // switch to PLAY state
+        me.state.change(me.state.PLAY);
+    }
+};
+
